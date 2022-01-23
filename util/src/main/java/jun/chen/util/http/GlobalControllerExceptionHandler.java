ad2dbb1276a7b9ea2b48dcf42f67d72153d65482
Jun <ccjh522@gmail.com>
2022-01-23 00:43:09 -0500

    added swagger documentation

--- util/src/main/java/jun/chen/util/http/GlobalControllerExceptionHandler.java
+++ util/src/main/java/jun/chen/util/http/GlobalControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package jun.chen.util.http;
 
+import jun.chen.util.exceptions.BadRequestException;
 import jun.chen.util.exceptions.InvalidInputException;
 import jun.chen.util.exceptions.NotFoundException;
 import org.slf4j.Logger;
@@ -11,8 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.HttpStatus.*;
 
 
 @RestControllerAdvice
@@ -32,6 +32,15 @@ public class GlobalControllerExceptionHandler {
     HttpErrorInfo handInvalidInputException(ServerHttpRequest request, Exception ex) {
         return createHttpErrorInfo(UNPROCESSABLE_ENTITY, request, ex);
     }
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(BadRequestException.class)
+    public @ResponseBody HttpErrorInfo handleBadRequestExceptions(
+            ServerHttpRequest request, BadRequestException ex) {
+
+        return createHttpErrorInfo(BAD_REQUEST, request, ex);
+    }
+
 
     private HttpErrorInfo createHttpErrorInfo(HttpStatus httpStatus, ServerHttpRequest request, Exception ex) {
         final String path = request.getPath().pathWithinApplication().value();
