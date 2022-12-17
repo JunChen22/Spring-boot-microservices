c02807f2c7e7269a13a0257f0583af6309f172fe
Jun <ccjh522@gmail.com>
2022-12-16 19:20:48 -0500

    added discovery service and double memory limit in docker container

--- spring-cloud/eureka-server/src/test/java/jun/chen/springcloud/eurekaserver/EurekaserverApplicationTests.java
+++ spring-cloud/eureka-server/src/test/java/jun/chen/springcloud/eurekaserver/EurekaserverApplicationTests.java
@@ -1,13 +1,39 @@
 package jun.chen.springcloud.eurekaserver;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class EurekaserverApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private TestRestTemplate testTemplate;
+
+	@Test
+	void catalogLoad() {
+		String expectedResponseBody = "{\"applications\":{\"versions__delta\":\"1\",\"apps__hashcode\":\"\",\"application\":[]}}";
+		ResponseEntity entity = testTemplate.getForEntity("/eureka/apps", String.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+		assertEquals(expectedResponseBody, entity.getBody());
+	}
+
+	@Test
+	void healthy() {
+		String expectedResponseBody = "{\"status\":\"UP\"}";
+		ResponseEntity<String> entity = testTemplate.getForEntity("/actuator/health", String.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+		assertEquals(expectedResponseBody, entity.getBody());
 	}
 
 }
