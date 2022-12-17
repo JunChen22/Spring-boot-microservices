d9d0fc9bec8e240bf25c58645137f89a87e61d3c
Jun <ccjh522@gmail.com>
2022-12-16 22:42:52 -0500

    added spring cloud gateway

--- spring-cloud/gateway/src/test/java/jun/chen/springcloud/gateway/GatewayApplicationTests.java
+++ spring-cloud/gateway/src/test/java/jun/chen/springcloud/gateway/GatewayApplicationTests.java
@@ -3,7 +3,13 @@ package jun.chen.springcloud.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT,
+		properties = {
+			"eureka.client.enabled=false"
+		}
+)
 class GatewayApplicationTests {
 
 	@Test
